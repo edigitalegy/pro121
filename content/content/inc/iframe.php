db40644f7aa337ccd76b79cae0774d0504aa9382
edigitalegy <130320276+edigitalegy@users.noreply.github.com>
2025-02-16 01:32:27 +0200

    Add files via upload

---
+++ content/content/inc/iframe.php
@@ -0,0 +1,16 @@
+<?php include_once './inc/header.php'; ?>
+
+<!--********************************************iframe******************************************************-->
+<div class="act1" style="text-align: center; top:0px;">
+  <iframe src="chatbot/index.html" width="640" height="640" frameborder="0" marginheight="0" marginwidth="0">انتظر</iframe> 
+</div>
+<div class="chat" style="text-align: center; top:0px;">
+  <iframe src="chat/chat.html" width="640" height="640" frameborder="0" marginheight="0" marginwidth="0">انتظر</iframe> 
+</div>
+<div class="posttest" style="text-align: center; top:0px;">
+  <iframe src="pretest.html" width="640" height="640" frameborder="0" marginheight="0" marginwidth="0">انتظر</iframe> 
+</div>
+<div class="pretest" style="text-align: center; top:0px;">
+  <iframe src="posttest.html" width="640" height="640" frameborder="0" marginheight="0" marginwidth="0">انتظر</iframe> 
+</div>
+<!--********************************************iframe******************************************************-->
