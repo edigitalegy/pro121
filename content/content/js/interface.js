056feed12f251b957b7707c7846dac4e30f4bbe9
edigitalegy <130320276+edigitalegy@users.noreply.github.com>
2025-03-07 21:00:29 +0200

    Update interface.js

--- content/content/js/interface.js
+++ content/content/js/interface.js
@@ -4,7 +4,6 @@ const muser = document.querySelector('.container .menu-list');
 const chatai = document.querySelector('.chatai');
 const menuList = document.querySelector(".container .menu-list");
 const userIcon = document.querySelector(".user");
-const adminbutton = document.getElementById('admin-button');
 /*************************************elements button******************************************/
 const btnpretest = document.getElementById('btnpretest');
 const btnposttest = document.getElementById('btnposttest');
